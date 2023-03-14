33f0455b5f38255a360506d108f8e55cedd3623f
Alex <alex.van.der.kroft@student.nhlstenden.com>
2023-03-14 13:57:36 +0100

    Added Board

--- DesignPatterns/src/Board.java
+++ DesignPatterns/src/Board.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Board {
+    GamePiece[][] pieces;
+
+    Board(){
+        pieces = new GamePiece[12][12];
+    }
+
+    public void movePiece(int posX, int posY){
+
+    }
+
+    public void turnPieceIntoKing(GamePiece piece){
+
+    }
+
+    public void acquirePieces(){
+
+    }
 }
