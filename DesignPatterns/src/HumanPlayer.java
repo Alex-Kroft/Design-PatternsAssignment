8fe09c0e56e6d3004c7575f3643c717e4fec9d2c
Aleksei Skorjak <43403562+AlekseiSkr@users.noreply.github.com>
2023-03-14 13:49:51 +0100

    boilerplate
    Empty code from the diagram

--- DesignPatterns/src/HumanPlayer.java
+++ DesignPatterns/src/HumanPlayer.java
@@ -1,2 +1,9 @@
-public class HumanPlayer {
+public class HumanPlayer extends Player {
+    public HumanPlayer(){
+
+    }
+
+    private void move(GamePiece piece, int PosX, int posY){
+        System.out.println("TEXT");
+    }
 }
