0ab24788c78c7570435dc85c83b5449a8b95d88e
HolyMelonSeed <sikora2012alex@gmail.com>
2023-03-14 13:44:11 +0100

    Added basic methods and parameters to piece-related classes

--- DesignPatterns/src/BaseDecorator.java
+++ DesignPatterns/src/BaseDecorator.java
@@ -1,2 +1,12 @@
-public class BaseDecorator {
+public abstract class BaseDecorator implements GamePiece {
+    private BasePiece wrappee;
+
+    public BaseDecorator(BasePiece basePiece) {
+
+    }
+
+
+    public abstract boolean checkIfCanMove();
+
+    public abstract boolean checkIfCanTake();
 }
---
+++ DesignPatterns/src/PiecePool.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class PiecePool {
+    private ArrayList<GamePiece> playerOnePieces;
+    private ArrayList<GamePiece> playerTwoPieces;
+
+    private PiecePool() {
+
+    }
+
+    static PiecePool getInstance() {
+        return null;
+    }
+
+    private void createPieces() {
+
+    }
+
+    public ArrayList<GamePiece> acquirePlayerOnePieces() {
+        return null;
+    }
+
+    public ArrayList<GamePiece> acquirePlayerTwoPieces() {
+        return null;
+    }
+
+}
